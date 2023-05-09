91821a6b9dae828172225a62b1fe00a106d63f48
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2023-05-09 16:54:34 +0300

    create intro

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ fuel = Fuel(texture_name=TEXTURE_NAMES['fuel'])
 heart = Heart(texture_name=TEXTURE_NAMES['heart'])
 
 background_sound = pygame.mixer.Sound("assets/sound/the-k9-background-music-By-tuna.voicemod.net.mp3")
+
+
 #########################################################################
 def getModel(path):
     if path not in factory:
@@ -201,26 +203,26 @@ def lighting():
 def draw_vehicle():
     global spaceship_position
     # fire tail outer oval
-    glPushMatrix() 
+    glPushMatrix()
     glColor(0.0784, 0.4235, 0.580)
-    glTranslate(spaceship_position-0.5, -0.1, abs(spaceship_position / 6)-5.5)
+    glTranslate(spaceship_position - 0.5, -0.1, abs(spaceship_position / 6) - 5.5)
     glRotate(5 * spaceship_position, 0, 0, 1)
     glScale(0.5, 0.5, 2)
-    glutSolidSphere(0.8,30,30)
-    glTranslate(2,0,0)
-    glutSolidSphere(0.8,30,30)
+    glutSolidSphere(0.8, 30, 30)
+    glTranslate(2, 0, 0)
+    glutSolidSphere(0.8, 30, 30)
     glPopMatrix()
     # fire tail inner oval
-    glPushMatrix() 
+    glPushMatrix()
     glColor(0.098, 0.6549, 0.8078)
-    glTranslate(spaceship_position-0.5, 0.1, abs(spaceship_position / 6)-5.5)
+    glTranslate(spaceship_position - 0.5, 0.1, abs(spaceship_position / 6) - 5.5)
     glRotate(5 * spaceship_position, 0, 0, 1)
     glScale(0.2, 0.45, 1.7)
-    glutSolidSphere(0.8,30,30)
-    glTranslate(5,0,0)
-    glutSolidSphere(0.8,30,30)
+    glutSolidSphere(0.8, 30, 30)
+    glTranslate(5, 0, 0)
+    glutSolidSphere(0.8, 30, 30)
     glPopMatrix()
-    #spaceship body
+    # spaceship body
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
     glEnable(GL_COLOR_MATERIAL)
@@ -317,17 +319,19 @@ def draw_fire():
     glScale(.3, .3, 3)
     glutSolidSphere(.5, 30, 30)
     glPopMatrix()
+
+
 def camera_setup():
-    global camera_coordinates,state
+    global camera_coordinates, state
     if state == 'intro':
         if camera_coordinates['x-eye'] <= 0:
-            camera_coordinates['x-eye'] += 0.5
-            camera_coordinates['z-eye'] -= 0.5
-            camera_coordinates['y_center'] += 11 / 50
-        print (camera_coordinates)
-        if camera_coordinates['x-eye'] == 0:
-            state='3'
-            print (state)
+            camera_coordinates['x-eye'] += 0.1
+            camera_coordinates['z-eye'] -= 0.1
+            camera_coordinates['y_center'] += 11 / 250
+        print(camera_coordinates)
+        if camera_coordinates['x-eye'] >= 0:
+            state = '3'
+            print(state)
     if state == "5":
         if camera_coordinates['y-eye'] < 50:
             camera_coordinates['y-eye'] += 0.5
@@ -348,46 +352,54 @@ def game():
     score = (generate // 100) * 100
     draw_text(f"SCORE: {score}", -.9, .7)
     draw_text("press P to pause ", -.9, .6, 4)
+    if state != 'intro':
+        if generate % 120 == 0:
+            speed = obstacles.generate_obstacle(num_of_rail=int(state), speed=speed)
+        obstacles.draw_obstacles(speed=speed)
+        num_of_heart, flash = obstacles.collision_detection(space_ship_position=spaceship_position,
+                                                            num_of_heart=num_of_heart, speed=speed,
+                                                            state=state, flash=flash)
 
+        if generate % 1920 == 0:
+            heart.generate_new_heart(num_of_rail=int(state), obstacles_x=obstacles.obstacle_x[-1], fuel_x=fuel.fuel_x)
+        heart.draw_old_heart(speed)
+        num_of_heart = heart.collision_detection(space_ship_position=spaceship_position, num_of_heart=num_of_heart,
+                                                 speed=speed, )
 
-    if generate % 120 == 0:
-        speed = obstacles.generate_obstacle(num_of_rail=int(state), speed=speed)
-    obstacles.draw_obstacles(speed=speed)
-    num_of_heart, flash = obstacles.collision_detection(space_ship_position=spaceship_position,
-                                                        num_of_heart=num_of_heart, speed=speed,
-                                                        state=state, flash=flash)
+        if 50 >= fuel_level >= 20 and not len(fuel.fuel_x):
+            fuel.generate_new_fuel(num_of_rail = int(state), obstacles_x=obstacles.obstacle_x[-1])
+        fuel.draw_old_fuel(speed=speed)
+        fuel_level = fuel.collision_detection(space_ship_position=spaceship_position, fuel_level=fuel_level, speed=speed)
 
-    if generate %  1920 == 0:
-        heart.generate_new_heart(num_of_rail=int(state), obstacles_x=obstacles.obstacle_x[-1], fuel_x=fuel.fuel_x)
-    heart.draw_old_heart(speed)
-    num_of_heart = heart.collision_detection(space_ship_position=spaceship_position, num_of_heart=num_of_heart,
-                                             speed=speed, )
+        if flash:
+            flash -= 1
+        if flash % 15 == 0:
+            draw_vehicle()
 
-    if 50 >= fuel_level >= 20 and not len(fuel.fuel_x):
-        fuel.generate_new_fuel(num_of_rail=int(state), obstacles_x=obstacles.obstacle_x[-1])
-    fuel.draw_old_fuel(speed=speed)
-    fuel_level = fuel.collision_detection(space_ship_position=spaceship_position, fuel_level=fuel_level, speed=speed)
-
-    if flash:
-        flash -= 1
-    if flash % 15 == 0:
-        draw_vehicle()
-
-    if speed < 3:
-        STEP = 3
-
+        if speed < 3:
+            STEP = 3
+        else:
+            STEP = 4
+        if generate >= 4000 and state == "3":
+            generate = 0
+            state = "5"
+        generate += STEP
+        fuel_generate += STEP
+        fuel_level -= 0.2
     else:
         draw_vehicle()
+
 
 #########################################################################
 def keyboard_callback(key, x, y):
     global state, pause, background_sound
     if key == b's' and state == "start":
-
         print(state)
-        state = "3"
+        state = 'intro'
+        print(state)
         background_sound.stop()
-        background_sound = pygame.mixer.Sound("assets/sound/Your Story Interactive - Love From Outer Space - Figting Game.mp3")
+        background_sound = pygame.mixer.Sound(
+            "assets/sound/Your Story Interactive - Love From Outer Space - Figting Game.mp3")
         background_sound.play(-1)
 
     if key == b'p':
@@ -400,19 +412,17 @@ def keyboard_callback(key, x, y):
 
 def mouse_callback(x, y):
     global spaceship_position, state
+    if state != 'intro':
+        spaceship_position = (-x + 750) / 45
+        if spaceship_position > 8 and state == '3':
+            spaceship_position = 8
+        elif spaceship_position < -8 and state == '3':
+            spaceship_position = -8
 
-    spaceship_position = (-x + 750) / 45
-    if spaceship_position > 8 and state == '3':
-        spaceship_position = 8
-    elif spaceship_position < -8 and state == '3':
-        spaceship_position = -8
-
-    if spaceship_position > 16 and state == '5':
-        spaceship_position = 16
-    elif spaceship_position < -16 and state == '5':
-        spaceship_position = -16
-
- 
+        if spaceship_position > 16 and state == '5':
+            spaceship_position = 16
+        elif spaceship_position < -16 and state == '5':
+            spaceship_position = -16
 
 
 #########################################################################
